33669984a90de457c95298b11026d8b3fd7ba10d
Sebastian <sebastianbecerra162004@gmail.com>
2025-06-28 10:27:39 -0400

    Edicion archivo de conexión

--- src/main/java/ConnectDB.java
+++ src/main/java/ConnectDB.java
@@ -2,9 +2,13 @@ import java.sql.*;
 
 public class ConnectDB {
     public static void main(String args[]) throws SQLException{
-
+        
+        String url = "jdbc:mysql://localhost:3306/";
+        String user = "root";
+        String pass = "xxx";
+        
         try {
-            Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/", "root", "");
+            Connection conexion = DriverManager.getConnection(url, user, pass);
         }
         catch(SQLException e) {
             return;
