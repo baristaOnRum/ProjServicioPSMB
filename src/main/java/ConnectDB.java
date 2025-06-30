2435b343cc50d0f007dab3eeb5dd641eb2e42973
Sebastian <sebastianbecerra162004@gmail.com>
2025-06-29 21:24:39 -0400

    Edicion archivo de conexión parte 2

--- src/main/java/ConnectDB.java
+++ src/main/java/ConnectDB.java
@@ -5,7 +5,7 @@ public class ConnectDB {
         
         String url = "jdbc:mysql://localhost:3306/mydb";
         String user = "root";
-        String pass = "xxx";
+        String pass = "bandidito10";
         Connection conexion = null;
         String sql = "INSERT INTO representante (" +
                 "cirepresentante, apellidos, nombres, estdciv, " +
