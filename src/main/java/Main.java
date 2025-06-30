9b5bb75e39678a23b872c7e12de83b5474eae487
Sebastian <sebastianbecerra162004@gmail.com>
2025-06-29 21:45:27 -0400

    Edicion archivo de conexión parte 4

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import forms.*;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame main = new planilla_inscripcion();
+        main.setVisible(true);
+
+    }
+}
