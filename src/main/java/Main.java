9aed5d671fd3f97228bb3b9e005c3963b987e2ec
Sebastian <sebastianbecerra162004@gmail.com>
2025-06-30 21:01:42 -0400

    Modificados errores de generación que impedían la corrida del código

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import forms.*;
 
 public class Main {
     public static void main(String[] args) {
-        planilla_inscripcion main = new planilla_inscripcion();
+        Menu_Principal main = new Menu_Principal();
         main.setVisible(true);
 
     }
--- src/main/java/forms/Menu_Principal.java
+++ src/main/java/forms/Menu_Principal.java
@@ -4,6 +4,8 @@
  */
 package forms;
 
+import java.util.Objects;
+
 /**
  *
  * @author Admin
