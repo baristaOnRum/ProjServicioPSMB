6acd7262f477f1dcba0753262231bfc1c72c18ad
Uzer <Uzer@PC-Raul>
2025-07-25 17:49:56 -0400

    guau guau

--- src/main/java/subsystems/individuos/autorizado.java
+++ src/main/java/subsystems/individuos/autorizado.java
@@ -5,14 +5,6 @@ public class autorizado extends personas{
     String tlf1;
     String tlf2;
 
-    autorizado(int ci, String nombres, String apellidos, String tlf1, String tlf2){
-        this.ci = ci;
-        this.nombres = nombres;
-        this.apellidos = apellidos;
-        this.tlf1 = tlf1;
-        this.tlf2 = tlf2;
-    }
-
     public int getCi() {
         return ci;
     }
