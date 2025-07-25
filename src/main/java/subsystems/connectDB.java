20a95d0e558b75c77a13d2f950fa33f0c95b414e
Sebastian <sebastianbecerra162004@gmail.com>
2025-07-25 15:21:55 -0400

    Modificadas clases

--- src/main/java/subsystems/connectDB.java
+++ src/main/java/subsystems/connectDB.java
@@ -30,7 +30,7 @@ public class connectDB {
                 "cirepresentante, apellidos, nombres, estdciv, " +
                 "`lugar(nac)`, `fecha(nac)`, nacionalidad, edad, " +
                 "`direccion(hab)`, `grado(estudios)`, ocupacion, " +
-                "`direccion(trabj)`, tlf, correo, parentesco, " +
+                "`direccion(trabj)`, tlf, correo, " +
                 "`niños menores de 6`) " +
                 "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
 
@@ -43,18 +43,18 @@ public class connectDB {
             query.setString(2, representante.getApellidos()); // apellidos
             query.setString(3, representante.getNombres()); // nombres
             query.setString(4, representante.getEstadoCivil()); // estdciv
-            query.setString(5, lugar_nac); // lugar_nac
-            query.setString(6, fecha_nac); // fecha_nac (formato YYYY-MM-DD es estándar)
-            query.setString(7, nacionalidad); // nacionalidad
-            query.setInt(8, edad); // edad
-            query.setString(9, direccion_hab); // direccion_hab
-            query.setString(10, grado_estudios); // grado_estudios
-            query.setString(11, ocupacion); // ocupacion
-            query.setString(12, direccion_trabj); // direccion_trabj
-            query.setLong(13, celular); // tlf
-            query.setString(14, correo); // correo
-            query.setString(15, rol); // parentesco
-            query.setInt(16, menores_6); // niños menores de 6
+            query.setString(5, representante.getLugarNac()); // lugar_nac
+            query.setDate(6, Date.valueOf(representante.getFechaNac())); // fecha_nac (formato YYYY-MM-DD es estándar)
+            query.setString(7, representante.getNacionalidad()); // nacionalidad
+            query.setInt(8, representante.getEdad()); // edad
+            query.setString(9, representante.getDireccionHab()); // direccion_hab
+            query.setString(10, representante.getGradoEstudios()); // grado_estudios
+            query.setString(11, representante.getOcupacion()); // ocupacion
+            query.setString(12, representante.getDireccionTrabj()); // direccion_trabj
+            query.setString(13, representante.getTlf1()); // tlf
+            query.setString(13, representante.getTlf2()); // tlf
+            query.setString(14, representante.getCorreo()); // correo
+            query.setBoolean(16, representante.isNinosMenor6()); // niños menores de 6
 
             // --- Ejecución de la consulta ---
             int columnasAfectadas = query.executeUpdate();
